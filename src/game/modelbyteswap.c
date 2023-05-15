c8a973fe5c0644952995bea9a0710b790455c7d4
doomhack <48415419+doomhack@users.noreply.github.com>
2023-05-15 01:18:45 +0100

    Model byteswap code.

--- src/game/modelbyteswap.c
+++ src/game/modelbyteswap.c
@@ -25,7 +25,72 @@
 
 //Models are stored in the ROM compressed so we have to byteswap them on load.
 
-void modelByteSwapModel(struct modeldef* modeldef, u32 baseAddr)
+#define MPTR(x) ((void*)((((u32)x) - offsetAddr) + baseAddr))
+
+void modelByteSwapNode(struct modelnode* node, u32 offsetAddr, u32 baseAddr)
+{
+	while (node)
+	{
+		node->type = BSWAP16(node->type);
+		node->rodata = BSWAP32((u32)node->rodata);
+		node->parent = BSWAP32((u32)node->parent);
+		node->next = BSWAP32((u32)node->next);
+		node->prev = BSWAP32((u32)node->prev);
+		node->child = BSWAP32((u32)node->child);
+
+		u32 type = node->type & 0xff;
+
+		switch (type)
+		{
+			case MODELNODETYPE_CHRINFO:
+				break;
+			case MODELNODETYPE_POSITION:
+				break;
+			case MODELNODETYPE_GUNDL:
+				break;
+			case MODELNODETYPE_DL:
+				break;
+			case MODELNODETYPE_DISTANCE:
+				break;
+			case MODELNODETYPE_TOGGLE:
+				break;
+			case MODELNODETYPE_REORDER:
+				break;
+			case MODELNODETYPE_11:
+				break;
+			case MODELNODETYPE_0B:
+				break;
+			case MODELNODETYPE_CHRGUNFIRE:
+				break;
+			case MODELNODETYPE_0D:
+				break;
+			case MODELNODETYPE_STARGUNFIRE:
+				break;
+			default:
+				break;
+		}
+
+		if (node->child)
+		{
+			node = MPTR(node->child);
+		}
+		else
+		{
+			while (node)
+			{
+				if (node->next)
+				{
+					node = MPTR(node->next);
+					break;
+				}
+
+				node = MPTR(node->parent);
+			}
+		}
+	}
+}
+
+void modelByteSwapModel(struct modeldef* modeldef, u32 offsetAddr)
 {
 	modeldef->rootnode = BSWAP32((u32)modeldef->rootnode);
 	modeldef->skel = BSWAP32((u32)modeldef->skel);
@@ -44,4 +109,14 @@ void modelByteSwapModel(struct modeldef* modeldef, u32 baseAddr)
 	{
 		p[i] = BSWAP32((u32)p[i]);
 	}
+
+	s16* partnums = (s16*)&modeldef->parts[modeldef->numparts];
+
+	for (int i = 0; i < modeldef->numparts; i++)
+	{
+		partnums[i] = BSWAP16(partnums[i]);
+	}
+
+	u32 baseAddr = (u32)modeldef;
+	modelByteSwapNode(MPTR(modeldef->rootnode), offsetAddr, baseAddr);
 }
